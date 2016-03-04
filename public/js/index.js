b0f7b05fac499b546302d039500b303818fa7daa
Gabriel Gaddi <ggaddi@ucsd.edu>
2016-03-03 21:47:48 -0800

    Added database functionality for login and sign up

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,11 @@
 		accountData = data;
 	} 
 }); */
+
+var database = new Firebase('https://vivid-heat-9192.firebaseio.com/');
+var dataRef = database.child("accounts");
+async: true;
+
 $(document).ready(function() {
 
 	var $email = $('<input type="text" name="email" placeholder="e-mail" class = "account" id= "email" />');
@@ -31,6 +36,12 @@ $(document).ready(function() {
 		}
 	});
 	$('#log').click(login);
+	dataRef.on("value", function(snapshot) {
+		var check = snapshot.val();
+ 		console.log(check);
+	}, function (errorObject) {
+  		console.log("The read failed: " + errorObject.code);
+	});
 });
 
 function login(e) {
@@ -42,26 +53,33 @@ function login(e) {
 function handleLogin(result){
 	//console.log(result['accounts'].length);
 	var value = 0;
-	var username;
+	var username;	
 	var password;
 	var email;
 	var allow = 0;
+/*	var database = new Firebase('https://vivid-heat-9192.firebaseio.com/');
+	var dataRef = database.child("accounts"); */
 	$("input[class=account]").each(function() {
 		if($('#log').text() == "log in!"){ 
-			if( value == 0) {
+			username = document.getElementById("username").value;
+			password = document.getElementById("password").value;
+			/*if( value == 0) {
 				username = $(this).val();
 				value = value +1;
 			}
 			else if (value == 1) {
 				password = $(this).val();
-			}
+			} */
     		//var email = $(this).attr("name");
     		//console.log(username);
     		//console.log(password);
     		//console.log("Break");
     	}
     	else{
-    		if( value == 0) {
+    		username = document.getElementById("username").value;
+    		password = document.getElementById("password").value;
+    		email = document.getElementById("email").value;
+    		/*if( value == 0) {
 				email = $(this).val();
 				value = value +1;
 			}
@@ -71,7 +89,7 @@ function handleLogin(result){
 			}
 			else if (value == 2){
 				password =  $(this).val();
-			}
+			} */
     		/*console.log(username);
     		console.log(password);
     		console.log(email); */
@@ -88,34 +106,42 @@ function handleLogin(result){
 			location.href = '/';
 		}
 		else {
-			for(var i = 0; i < result['accounts'].length; i++){
-				if(result.accounts[i].username == username && result.accounts[i].password == password) {
-					allow = 1;
-					localStorage.setItem("user", username);
-					if(localStorage.getItem(username) == null) {
-						localStorage.setItem( username, JSON.stringify(result.accounts[i]));
+			dataRef.once("value", function(snapshot) {
+				snapshot.forEach(function(data) {
+					//console.log(data.key());
+					if(data.val().username == username && data.val().password == password) {
+						allow = 1;
+						//console.log("I am working");
+						localStorage.setItem("key", data.key());
+						localStorage.setItem("user", username);
+						if(localStorage.getItem(username) == null) {
+							localStorage.setItem( username, JSON.stringify(data.val()));
+						}
+						return;
 					}
-					break;
+  				});
+				if(allow == 1) {
+					location.href = 'home';
 				}
-			}
-			if(allow == 1) {
-				location.href = 'home';
-			}
-			else{
-				location.href = '/';
-			}
+				else{
+					location.href = '/';
+				}
 
-			/*jsonObj = [];
-			item = {};
-    		item ["username"] = username;
-    		item ["password"] = password;
+				/*jsonObj = [];
+				item = {};
+    			item ["username"] = username;
+    			item ["password"] = password;
 
-    		jsonObj.push(item);
-			jsonString = JSON.stringify(jsonObj);
-			//console.log("I am running"); */
-			//console.log(jsonString);
-			//location.href = 'home.html';
+	    		jsonObj.push(item);
+				jsonString = JSON.stringify(jsonObj);
+				//console.log("I am running"); */
+				//console.log(jsonString);
+				//location.href = 'home.html';
+			}, 		function (errorObject) {
+  				console.log("The read failed: " + errorObject.code);
+			});
 		}
+		
 	}
 	else {
 		if(username == "" || password == "" || email == ""){
@@ -132,6 +158,13 @@ function handleLogin(result){
 				"currency": 1000,
 				"message": "enter a personal message here..."
 			};
+			dataRef.push(jsonObj);
+			/*var check = dataRef.set('User ' + "hi" + ' says ' + "bye" + " I am slow");
+			dataRef.set('User ' + "hi" + ' says ' + "bye" + " I am slow2");
+			database.set('User ' + "hi" + ' says ' + "bye" + " I am slow3");
+			console.log("check here");
+			console.log(check); */
+			//dataRef.push(jsonObj);
     		/*item ["username"] = username;
     		item ["password"] = password;
     		item ["email"] = email;
@@ -139,6 +172,7 @@ function handleLogin(result){
     		jsonObj.push(item); */
     		//result["accounts"].push(jsonObj);
     		console.log(result['accounts'].length);
+
     		/*$.getJSON('./accounts.json', function(data) {
 				console.log(data);
 				result["accounts"].push(jsonObj);
